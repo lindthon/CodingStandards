f8f9b7fff425a80840b150778a712f23c923bec5
lindthon <lindthon4@hotmail.com>
2020-06-21 16:31:49 -0500

    Project, Lab report and captures of the checkstyle added

---
+++ src/main/java/labcodingstandards/Calculator.java
@@ -0,0 +1,85 @@
+// Copyright (C) 2020
+// All rights reserved
+
+package labcodingstandards;
+
+import java.util.Scanner;
+
+
+/**
+ * @author Lindthon Iparreño
+ */
+public class Calculator {
+/**
+ * @param args
+ */
+//CHECKSTYLE:OFF
+public static void main(final String[] args) {
+//CHECKSTYLE:ON
+Scanner reader = new Scanner(System.in);
+
+        System.out.print("1. +\n2. -\n3. *\n4. /\nEnter an operator: ");
+
+        char operator = reader.nextLine().charAt(0);
+        double first;
+        double second;
+        String input;
+
+        while (true) {
+        System.out.print("Enter first number: ");
+        input = reader.nextLine();
+
+            try {
+            	first = Integer.parseInt(input);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Not valid!");
+            }
+        }
+
+        while (true) {
+        	System.out.print("Enter second number: ");
+        	input = reader.nextLine();
+
+            try {
+            	second = Integer.parseInt(input);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Not valid!");
+            }
+        }
+
+        Calculator cal = new Calculator();
+        String result = cal.operation(first, second, operator);
+
+        System.out.printf(result);
+		reader.close();
+	}
+
+/**
+ * @param first The first double to make the operation
+ * @return The value of the operation
+ */
+	private String operation(double first, double second, char operator) {
+
+		double result = 0;
+		switch (operator) {
+            case '1':
+                result = first + second;
+                break;
+            case '2':
+                result = first - second;
+                break;
+            case '3':
+                result = first * second;
+                break;
+            case '4':
+                result = first / second;
+                break;
+            default:
+            	return "Error! operator is not correct";
+        }
+		return "The result is: " + result;
+	}
+}
+
